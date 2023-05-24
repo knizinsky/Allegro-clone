f600cac79396c5214300e254ebbb534f7f974629
knizinsky <konradnizinski20@gmail.com>
2023-05-24 22:15:36 +0200

    Categories section added

--- allegro-clone/src/App.js
+++ allegro-clone/src/App.js
@@ -1,13 +1,15 @@
 import "./App.module.scss";
 import Coupon from "./Components/Coupon/Coupon";
 import Navigation from "./Components/Navigation/Navigation";
+import Categories from "./Components/Categories/Categories";
 
 function App() {
 	return (
 		<div className="App" id="home">
-			<Coupon></Coupon>
+			<Coupon/>
 			<header>
-				<Navigation></Navigation>
+				<Navigation/>
+				<Categories/>
 			</header>
 			<main style={{height: '200vh'}}></main>
 		</div>
---
+++ allegro-clone/src/Components/Categories/Categories.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './Categories.module.scss'
+import arrow from '../../Img/action-arrowhead.svg'
+
+const Categories = () => {
+  return (
+    <div className={styles.categoriesWrapper}>
+        <button className={styles.categoriesBtn}>
+            Kategorie
+            <img src={arrow} alt="Strzałka" className={styles.actionArrow}/>
+        </button>
+        <ul className={styles.categoriesList}>
+            <li>Strefa Okazji</li>
+            <li>Promocje z Monetami</li>
+            <li>Premiery</li>
+            <li>Allegro inspiruje</li>
+            <li>Sprzedawaj na allegro</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Categories
