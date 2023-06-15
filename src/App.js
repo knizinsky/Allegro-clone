2bdd47fe8a99d7d228aee9ec03dc27c5a545000a
knizinsky <konradnizinski20@gmail.com>
2023-06-15 19:14:06 +0200

    Added benefits section

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { useState } from "react";
 import FooterMini from "./Components/FooterMini/FooterMini";
 import GroupServices from "./Components/GroupServices/GroupServices";
 import FooterLinks from "./Components/FooterLinks/FooterLinks";
+import StaticSection from "./Components/StaticSection/StaticSection";
 
 function App() {
 	const [currentTotalItems, setCurrentTotalItems] = useState(0);
@@ -38,6 +39,7 @@ function App() {
 				<Categories />
 				<main>
 					<Slider />
+					<StaticSection/>
 					<ItemsSection title="Znajdź idealny prezent" specialImg={true} section={1}></ItemsSection>
 					<ItemsSection title="Mogą Cię zainteresować" specialImg={false} section={2}></ItemsSection>
 				</main>
---
+++ src/Components/StaticSection/StaticSection.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./StaticSection.module.scss";
+import allegroPay from "../../Img/SpecialOffers/allegro-pay.svg";
+import allegroSmart from "../../Img/SpecialOffers/allegro-smart.svg";
+import coin from "../../Img/SpecialOffers/coin.svg";
+import eBilet from "../../Img/SpecialOffers/eBilet.svg";
+import guaranteed from "../../Img/SpecialOffers/guaranteed.svg";
+import rocket from "../../Img/SpecialOffers/rocket.svg";
+
+const StaticSection = () => {
+	return (
+		<div className={styles.container}>
+			<h2>Odkrywaj korzyści</h2>
+			<div className={styles.boxContainer}>
+				<div className={styles.smartBox}>
+					<img src={allegroSmart} alt="Smart" /> Zarządzaj Smart!
+				</div>
+				<div className={styles.rrsoBox}>
+					<img src={allegroPay} alt="Kup teraz, płać później. RRSO 0%" />
+					Kup teraz, płać później. RRSO 0%
+				</div>
+				<div className={styles.guaranteeBox}>
+					<img src={guaranteed} alt="Gwarancja najniższej ceny" />
+					Gwarancja najniższej ceny
+				</div>
+				<div className={styles.coinsBox}>
+					<img src={coin} alt="Zgarniaj monety" />
+					Zgarniaj monety
+				</div>
+				<div className={styles.choiceBox}>
+					<img src={eBilet} alt="Największy wybór biletów" />
+					Największy wybór biletów
+				</div>
+				<div className={styles.newBox}>
+					<img src={rocket} alt="Zobacz, co nowego" />
+					Zobacz, co nowego
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default StaticSection;
