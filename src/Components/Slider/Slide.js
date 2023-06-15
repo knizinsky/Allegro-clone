1512563e8fa6eca3a8a6bf9ff7dee959c76197e1
knizinsky <konradnizinski20@gmail.com>
2023-06-15 14:16:37 +0200

    Slider photos added

--- src/Components/Slider/Slide.js
+++ src/Components/Slider/Slide.js
@@ -4,8 +4,8 @@ import styles from "./Slide.module.scss";
 const Slide = (props) => {
 	return (
 		<div className={styles.slideWrapper}>
-			<h2 className={styles.mainText}>{props.mainText}</h2>
-			<p className={styles.subText}>{props.subText}</p>
+			<h2 className={styles.mainText} style={{backgroundColor: props.mainTextBgColor, color: props.mainTextColor}}>{props.mainText}</h2>
+			<p className={styles.subText} style={{backgroundColor: props.subTextBgColor, color: props.subTextColor}}>{props.subText}</p>
 			<img src={props.img} alt={props.alt} />
 		</div>
 	);
--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -3,6 +3,9 @@ import styles from "./Slider.module.scss";
 import Slide from "./Slide";
 import holidayPromo from "../../Img/holiday-promo.webp";
 import SliderBtn from "../SliderBtn/SliderBtn";
+import garnierPromo from '../../Img/garnier-dni-piekna.webp'
+import fatherDayPromo from '../../Img/father-day-promo.webp'
+import garnierPromo2 from '../../Img/garnier-promo2.webp'
 
 const Slider = () => {
 	const [sliderPosition, setSliderPosition] = useState(0);
@@ -41,24 +44,28 @@ const Slider = () => {
 						alt="Reklama wakacyjna"
 						mainText="Niskie ceny na początek lata mają"
 						subText="Przygotuj się na wakacje"
+						mainTextColor="white"
+						subTextColor="black"
+						mainTextBgColor="#00a790"
+						subTextBgColor="#fac314"
 					></Slide>
 					<Slide
-						img={holidayPromo}
-						alt="Reklama wakacyjna"
-						mainText="Niskie ceny na początek lata mają"
-						subText="Przygotuj się na wakacje"
+						img={garnierPromo}
+						alt="Reklama garnier"
 					></Slide>
 					<Slide
-						img={holidayPromo}
-						alt="Reklama wakacyjna"
-						mainText="Niskie ceny na początek lata mają"
-						subText="Przygotuj się na wakacje"
+						img={fatherDayPromo}
+						alt="Reklama na dzień ojca"
+						mainText="Dzień Ojca"
+						subText="Prezenty w wymarzonych cenach"
+						mainTextColor="black"
+						subTextColor="white"
+						mainTextBgColor="#fff"
+						subTextBgColor="#0d1e28"
 					></Slide>
 					<Slide
-						img={holidayPromo}
-						alt="Reklama wakacyjna"
-						mainText="Niskie ceny na początek lata mają"
-						subText="Przygotuj się na wakacje"
+						img={garnierPromo2}
+						alt="Reklama garnier"
 					></Slide>
 				</div>
 			</div>
