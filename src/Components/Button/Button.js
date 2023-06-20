932ffcb48a256b75d94f17df2f21ad77a6e208a1
knizinsky <konradnizinski20@gmail.com>
2023-06-20 20:35:22 +0200

    login button problem fixed

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,24 +1,35 @@
-import React, { useContext } from 'react'
-import styles from './Button.module.scss'
-import { CartContext } from '../../Contexts/CartContext';
+import React, { useContext } from "react";
+import styles from "./Button.module.scss";
+import { CartContext } from "../../Contexts/CartContext";
 
 const Button = (props) => {
-  let cartContext = useContext(CartContext);
+	let cartContext = useContext(CartContext);
 
-  const clickHandler = () => {
-    cartContext.updatePrice(cartContext.currentPrice + parseFloat(props.price.replace(",", ".")))
-    cartContext.updateTotalItems(cartContext.currentTotalItems + 1)
-    cartContext.updateItemsList({
-      key: props.key,
-      price: props.price,
-      img: props.img,
-      title: props.title
-    })
-  }
+	const clickHandler = () => {
+		if (props.price) {
+			cartContext.updatePrice(
+				cartContext.currentPrice + parseFloat(props.price.replace(",", "."))
+			);
+			cartContext.updateTotalItems(cartContext.currentTotalItems + 1);
+			cartContext.updateItemsList({
+				key: props.key,
+				price: props.price,
+				img: props.img,
+				title: props.title,
+			});
+		}
+	};
 
-  return (
-    <button type={props.type} className={styles.btn} style={props.style} onClick={clickHandler}>{props.value}</button>
-  )
-}
+	return (
+		<button
+			type={props.type}
+			className={styles.btn}
+			style={props.style}
+			onClick={clickHandler}
+		>
+			{props.value}
+		</button>
+	);
+};
 
-export default Button
+export default Button;
